8e1479a82dbc08383988bf9ba87e442605b94e5f
tud8951 <tud8951@163.com>
2025-05-04 14:37:31 +0800

    new

--- script.js
+++
@@ -1,8 +0,0 @@
-function pay(method) {
-  if (method === 'wechat') {
-    // 直接跳转到 pay.html 页面
-    window.location.href = "pay.html";
-  } else if (method === 'usdt') {
-    alert("请使用USDT支付：\n地址：0xAdeB8A7967c59bC7E99b15E3884D3bb3FbAb6645);
-  }
-}
