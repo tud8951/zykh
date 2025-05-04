dedc660ab1658f5ef2cf0ab800c86d04abaa48c3
tud8951 <111268477+tud8951@users.noreply.github.com>
2025-05-04 14:12:03 +0800

    new

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function pay(method) {
     // 直接跳转到 pay.html 页面
     window.location.href = "pay.html";
   } else if (method === 'usdt') {
-    alert("请使用USDT支付：\n地址：TRX1234567890ABCDEF\n金额：14.99 USDT");
+    alert("请使用USDT支付：\n地址：0xAdeB8A7967c59bC7E99b15E3884D3bb3FbAb6645);
   }
 }
